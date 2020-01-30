4bdcb6d7a50f102f7b6454fb8224cab05b4b077a
Maurício Pimenta <mjbpimenta@gmail.com>
2020-01-30 16:06:41 -0300

    Adding test to app bar in page weather of today, finding add button in app bar.

--- test/weather_today/weather_today_page_test.dart
+++ test/weather_today/weather_today_page_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app/weather_today/weather_today_page.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -20,8 +13,20 @@ void main() {
     await tester.pumpWidget(materialApp);
 
     final textToShow = 'São Pedro Da Aldeia';
-
     final titleFinder = find.text(textToShow);
     expect(titleFinder, findsOneWidget);
+  });
+
+  testWidgets('add button in app bar expected', (WidgetTester tester) async {
+    final materialApp = MaterialApp(
+      home: Scaffold(
+        appBar: weatherTodayAppBar(),
+      ),
+    );
+
+    await tester.pumpWidget(materialApp);
+
+    final addButton = find.byIcon(Icons.add);
+    expect(addButton, findsOneWidget);
   });
 }
