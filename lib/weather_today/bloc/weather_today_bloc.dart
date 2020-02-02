095dca04c2189534109cf80c2a048c66a46a7ded
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-02 19:12:25 -0300

    Creating weather today bloc, mapping some events to state.

---
+++ lib/weather_today/bloc/weather_today_bloc.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_app/weather/repository/weather_repository.dart';
+import 'package:flutter_app/weather_today/bloc/weather_today_event.dart';
+import 'package:flutter_app/weather_today/bloc/weather_today_state.dart';
+import 'package:meta/meta.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null);
+
+  @override
+  WeatherState get initialState => WeatherEmpty();
+
+  @override
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    if (event is FetchWeather) {
+      yield* _mapFetchWeatherToState(event);
+    } else if (event is RefreshWeather) {
+      yield* _mapRefreshWeatherToState(event);
+    }
+  }
+
+  Stream<WeatherState> _mapFetchWeatherToState(FetchWeather event) async* {
+    yield WeatherLoading();
+    try {
+      final weather = await weatherRepository.getWeatherFromLocation(
+        event.locationId,
+      );
+      yield WeatherLoaded(weather: weather);
+    } catch (_) {
+      yield WeatherError();
+    }
+  }
+
+  Stream<WeatherState> _mapRefreshWeatherToState(RefreshWeather event) async* {
+    try {
+      final weather = await weatherRepository.getWeatherFromLocation(
+        event.locationId,
+      );
+
+      yield WeatherLoaded(weather: weather);
+    } catch (_) {
+      yield WeatherError();
+    }
+  }
+}
