c8a3d40f8a4acf04500476bc34318319787be680
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-07 22:38:12 -0300

    Adding and initiating hive in project.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,14 @@ import 'package:flutter_app/permission/location_permission_bloc.dart';
 import 'package:flutter_app/weather/repository/weather_repository.dart';
 import 'package:flutter_app/weather_today/bloc/weather_today_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  initiatingNoSqlHive();
+
   final metaWeatherApiClient = MetaWeatherApiClient(
     httpClient: http.Client(),
   );
@@ -38,4 +42,9 @@ void main() {
   );
 
   runApp(multiBlocProvider);
+}
+
+initiatingNoSqlHive() async {
+  var dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
 }
