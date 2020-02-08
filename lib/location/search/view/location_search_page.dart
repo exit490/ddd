89cff6e333b0212645a31947316608d8be56ad84
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-08 20:19:43 -0300

    Updating search page, removing unnecessary codes.

--- lib/location/search/view/location_search_page.dart
+++ lib/location/search/view/location_search_page.dart
@@ -1,84 +1,48 @@
 import 'package:flutter/material.dart';
 
-class SearchLocationPage extends StatefulWidget {
-  SearchLocationPage({Key key}) : super(key: key);
-
-  @override
-  _SearchLocationPageState createState() => new _SearchLocationPageState();
-}
-
-class _SearchLocationPageState extends State<SearchLocationPage> {
-  final key = new GlobalKey<ScaffoldState>();
+class SearchLocationPage extends StatelessWidget {
   final TextEditingController _searchQuery = new TextEditingController();
-  List<String> _list;
-  bool _isSearching;
-  String _searchText = "";
-
-  _SearchLocationPageState() {
-    _searchQuery.addListener(() {
-      if (_searchQuery.text.isEmpty) {
-        setState(() {
-          _isSearching = false;
-          _searchText = "";
-        });
-      } else {
-        setState(() {
-          _isSearching = true;
-          _searchText = _searchQuery.text;
-        });
-      }
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _isSearching = false;
-    init();
-  }
-
-  void init() {
-    _list = List();
-    _list.add("Google");
-    _list.add("IOS");
-    _list.add("Andorid");
-    _list.add("Dart");
-    _list.add("Flutter");
-    _list.add("Python");
-    _list.add("React");
-    _list.add("Xamarin");
-    _list.add("Kotlin");
-    _list.add("Java");
-    _list.add("RxAndroid");
-  }
 
   @override
   Widget build(BuildContext context) {
+    bool _isSearching = false;
+    String _searchText = "";
+
+    _searchQuery.addListener(() {
+      if (_searchQuery.text.isEmpty) {
+        _isSearching = false;
+        _searchText = "";
+      } else {
+        _isSearching = true;
+        _searchText = _searchQuery.text;
+      }
+    });
+
     return Scaffold(
       key: key,
       appBar: buildBar(context),
       body: new ListView(
         padding: new EdgeInsets.symmetric(vertical: 8.0),
-        children: _isSearching ? _buildSearchList() : _buildList(),
+        children: _isSearching ? _buildSearchList(_searchText) : _buildList(),
       ),
     );
   }
 
   List<ChildItem> _buildList() {
-    return _list
+    return getList()
         .map(
           (contact) => new ChildItem(contact),
         )
         .toList();
   }
 
-  List<ChildItem> _buildSearchList() {
+  List<ChildItem> _buildSearchList(_searchText) {
     if (_searchText.isEmpty) {
-      return _list.map((contact) => new ChildItem(contact)).toList();
+      return getList().map((contact) => new ChildItem(contact)).toList();
     } else {
       List<String> _searchList = List();
-      for (int i = 0; i < _list.length; i++) {
-        String name = _list.elementAt(i);
+      for (int i = 0; i < getList().length; i++) {
+        String name = getList().elementAt(i);
         if (name.toLowerCase().contains(_searchText.toLowerCase())) {
           _searchList.add(name);
         }
@@ -112,6 +76,22 @@ class _SearchLocationPageState extends State<SearchLocationPage> {
             )),
       ),
     );
+  }
+
+  List<String> getList() {
+    final List<String> _list = List();
+    _list.add("Google");
+    _list.add("IOS");
+    _list.add("Andorid");
+    _list.add("Dart");
+    _list.add("Flutter");
+    _list.add("Python");
+    _list.add("React");
+    _list.add("Xamarin");
+    _list.add("Kotlin");
+    _list.add("Java");
+    _list.add("RxAndroid");
+    return _list;
   }
 }
 
