aa4c2bcf38aee58327ac6ceda4dfb5d0404dd2db
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-08 19:44:02 -0300

    Adding method to fetch locations by city name

--- lib/meta_weather/meta_weather_api_client.dart
+++ lib/meta_weather/meta_weather_api_client.dart
@@ -26,6 +26,26 @@ class MetaWeatherApiClient {
     return (locationJson.first)['woeid'];
   }
 
+  Future<List<LocationModel>> fetchLocationsByCityName(String city) async {
+    final locationUrl = '$baseUrl/api/location/search/?query=$city';
+    final locationResponse = await this.httpClient.get(locationUrl);
+
+    if (locationResponse.statusCode != 200) {
+      throw Exception('error getting locationId for city');
+    }
+
+    final locationJsonList = jsonDecode(locationResponse.body) as List;
+
+    List<LocationModel> locationModelList = [];
+
+    for (final locationJson in locationJsonList) {
+      final locationModel = LocationModel.fromJson(locationJson);
+      locationModelList.add(locationModel);
+    }
+
+    return locationModelList;
+  }
+
   Future<Weather> fetchWeather(int locationId) async {
     final weatherUrl = '$baseUrl/api/location/$locationId';
     final weatherResponse = await this.httpClient.get(weatherUrl);
