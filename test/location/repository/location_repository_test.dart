94f6ccee4daef44bdfc01c7d075b327d37bec4da
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-09 23:50:58 -0300

    adding test to local repository.

--- test/location/repository/location_repository_test.dart
+++ test/location/repository/location_repository_test.dart
@@ -60,4 +60,28 @@ main() {
 
     expect(mockedLocationList()[0], location);
   });
+
+  test('if locations is empty position is expected', () async {
+    when(
+      geoLocationApiClientMocked.getMyLocation(),
+    ).thenAnswer(
+      (_) => Position(latitude: 1, longitude: 2),
+    );
+
+    when(
+      metaWeatherApiClientMocked.fetchLocationsByLatLong(1, 2),
+    ).thenAnswer(
+      (_) => Future.value([]),
+    );
+
+    final location = await locationRepository.buildDefaultLocation();
+    final locationModel = LocationModel(
+      title: 'Rio de Janeiro',
+      locationType: 'City',
+      woeid: 455825,
+      latLong: '-22.976730,-43.195080',
+    );
+
+    expect(locationModel, location);
+  });
 }
