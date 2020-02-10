f474c32e87cb45f5bc613c9bcb41ec8bd29c0c4b
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-10 01:45:36 -0300

    updating assets path

--- lib/weather/view/weather_backgroud.dart
+++ lib/weather/view/weather_backgroud.dart
@@ -4,11 +4,10 @@ class WeatherBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      "assets/images/cloudy_1.jpg",
+      "assets/images/cloudy/1.jpg",
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       fit: BoxFit.cover,
     );
-    ;
   }
 }
