0e9cbf0145abd92716f62279fb4e054d5493c33b
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-10 02:00:16 -0300

    Adding background image with opacity,

--- lib/weather/view/weather_backgroud.dart
+++ lib/weather/view/weather_backgroud.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class WeatherBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      "assets/images/cloudy/1.jpg",
+    final images = Image.asset(
+      "assets/images/clear/1.jpg",
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       fit: BoxFit.cover,
+    );
+
+    return Stack(
+      children: <Widget>[
+        images,
+        Container(
+          color: Colors.black26,
+        )
+      ],
     );
   }
 }
