702b4063afcf7cdae2b3917c3c48749080f1cafe
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-15 01:14:45 -0300

    Removing tests to location repository.

--- test/location/repository/location_repository_test.dart
+++ test/location/repository/location_repository_test.dart
@@ -1,37 +1,27 @@
-import 'package:flutter_app/location/geo_location/geo_location_client.dart';
-import 'package:flutter_app/location/model/location_model.dart';
-import 'package:flutter_app/location/no_sql/location_nosql_client.dart';
-import 'package:flutter_app/location/repository/location_repository.dart';
-import 'package:flutter_app/meta_weather/meta_weather_api_client.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:mockito/mockito.dart';
 
-import '../location_repository_mocked_util.dart';
-
-class _GeoLocationApiClientMocked extends Mock implements GeoLocationApiClient {
-}
-
-class _MetaWeatherApiClientMocked extends Mock implements MetaWeatherApiClient {
-}
-
-class _LocationNoSqlClientMocked extends Mock implements LocationNoSqlClient {}
+//class _GeoLocationApiClientMocked extends Mock implements GeoLocationApiClient {
+//}
+//
+//class _MetaWeatherApiClientMocked extends Mock implements MetaWeatherApiClient {
+//}
+//
+//class _LocationNoSqlClientMocked extends Mock implements LocationNoSqlClient {}
 
 main() {
-  _MetaWeatherApiClientMocked metaWeatherApiClientMocked;
-  _GeoLocationApiClientMocked geoLocationApiClientMocked;
-  _LocationNoSqlClientMocked locationNoSqlClientMocked;
-  LocationRepository locationRepository;
+//  _MetaWeatherApiClientMocked metaWeatherApiClientMocked;
+//  _GeoLocationApiClientMocked geoLocationApiClientMocked;
+//  _LocationNoSqlClientMocked locationNoSqlClientMocked;
 
   setUp(() {
-    metaWeatherApiClientMocked = _MetaWeatherApiClientMocked();
-    geoLocationApiClientMocked = _GeoLocationApiClientMocked();
-    locationNoSqlClientMocked = _LocationNoSqlClientMocked();
-    locationRepository = LocationRepository(
-      locationNoSqlClient: locationNoSqlClientMocked,
-      geoLocationApiClient: geoLocationApiClientMocked,
-      metaWeatherApiClient: metaWeatherApiClientMocked,
-    );
+//    metaWeatherApiClientMocked = _MetaWeatherApiClientMocked();
+//    geoLocationApiClientMocked = _GeoLocationApiClientMocked();
+//    locationNoSqlClientMocked = _LocationNoSqlClientMocked();
+//    locationRepository = LocationRepository(
+//      locationNoSqlClient: locationNoSqlClientMocked,
+//      geoLocationApiClient: geoLocationApiClientMocked,
+//      metaWeatherApiClient: metaWeatherApiClientMocked,
+//    );
   });
 
   test('if location from my position is expected', () async {
