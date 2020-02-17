b010db32519ed3dac28d4b47a6bcb537214babbe
Maurício Pimenta <mjbpimenta@gmail.com>
2020-02-17 02:03:46 -0300

    Removing unused import.

--- lib/search/bloc/search_location_state.dart
+++ lib/search/bloc/search_location_state.dart
@@ -1,6 +1,4 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_app/location/model/location_model.dart';
 
 abstract class SearchLocationState extends Equatable {
   const SearchLocationState();
